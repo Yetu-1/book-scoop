fc5281571b597ee4733bc3ec9f7141f2ceabd41c
David-Salihu <davidsalihu19@gmail.com>
2024-04-10 13:21:10 +0100

    Add database

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import pg from "pg"
+import { password } from "pg/lib/defaults";
+
+const db = pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "books",
+    password: "swordfish",
+    port: 5432,
+});
+
+db.connect();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
