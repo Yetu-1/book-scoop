64c3bfdda1ae18df95e6fd3e4935613f58f979a8
David-Salihu <davidsalihu19@gmail.com>
2024-04-10 14:55:32 +0100

    Implement database read

--- index.js
+++ index.js
@@ -11,13 +11,20 @@ const db = new pg.Client({
 
 db.connect();
 
+let books = []; 
+
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    try{
+        const result = await db.query("SELECT * FROM book_info");
+        res.render("index.ejs", {books: result.rows});
+    }catch(err) {
+        console.log(err);
+    }
 });
 
 app.listen(port, () => {
