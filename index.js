f53f08894969ada10b6c81b41323b6480b205bcb
David-Salihu <davidsalihu19@gmail.com>
2024-04-11 22:04:41 +0100

    implement database add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import pg from "pg";
+import bodyParser from "body-parser"
 
 const db = new pg.Client({
     user: "postgres",
@@ -17,6 +18,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
     try{
@@ -25,6 +27,22 @@ app.get("/", async (req, res) => {
     }catch(err) {
         console.log(err);
     }
+});
+
+app.post("/add", async (req, res) => {
+    console.log(req.body);
+    const date = new Date();
+    try{
+        await db.query("INSERT INTO book_info (title, review, rating, date) VALUES ($1, $2, $3, $4);", 
+        [req.body.title, req.body.review, req.body.rating, date]);
+    }catch(err) {
+        console.log(err);
+    }
+    res.redirect("/");
+});
+
+app.post("/new", (req, res) => {
+    res.render("new.ejs");
 });
 
 app.listen(port, () => {
