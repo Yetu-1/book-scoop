7420e936f63195105a93f85451e28cf0f794b79a
David-Salihu <davidsalihu19@gmail.com>
2024-04-12 12:53:49 +0100

    Change layout and css

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", async (req, res) => {
     try{
         const result = await db.query("SELECT * FROM book_info");
-        res.render("index.ejs", {books: result.rows});
+        books = result.rows;
+        res.render("index.ejs", {books: books});
     }catch(err) {
         console.log(err);
     }
@@ -40,7 +41,7 @@ app.post("/add", async (req, res) => {
     }
 });
 
-app.post("/new", (req, res) => {
+app.get("/new", (req, res) => {
     res.render("new.ejs");
 });
 
@@ -48,6 +49,24 @@ app.post("/delete", async (req, res) => {
     const id = req.body.delete;
     try{
         await db.query("DELETE FROM book_info WHERE id = $1;", [id]);
+        res.redirect("/");
+    }catch(err) {
+        console.log(err);
+    }
+});
+
+app.post("/edit", async (req, res) => {
+    const book_id = parseInt(req.body.edit);
+    const book_to_edit = books.find((book) => book.id = book_id);
+    console.log(book);
+    res.render("new.ejs", {heading: "Edit Review", book: book_to_edit})
+});
+
+app.post("/update", async (req, res) => {
+    const date = new Date();
+    try{
+        await db.query("INSERT INTO book_info (title, review, rating, date) VALUES ($1, $2, $3, $4);", 
+        [req.body.title, req.body.review, req.body.rating, date]);
         res.redirect("/");
     }catch(err) {
         console.log(err);
